1fcdae6b9b48943347e447c98166ec21e7290e88
your_username <raselkazi080@gmail.com>
2022-01-30 20:27:05 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const app = require("express")();
+const httpServer = require("http").createServer(app);
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+const PORT = 7000;
+
+app.get("/", (req, res) => {
+  res.status(200).json({ name: "Server" });
+});
+
+const users = {};
+
+io.on("connection", (socket) => {
+  socket.on("disconnect", () => {
+    for (let user in users) {
+      if (users[user] === socket.id) {
+        delete users[user];
+      }
+    }
+    io.emit("all_users", users);
+  });
+
+  socket.on("new_user", (username) => {
+    users[username] = socket.id;
+    //we can tell every other users someone connected
+    io.emit("all_users", users);
+  });
+
+  socket.on("send_message", (data) => {
+    const socketId = users[data.receiver];
+    io.to(socketId).emit("new_message", data);
+  });
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`Server Listening on port ${PORT}`);
+});
